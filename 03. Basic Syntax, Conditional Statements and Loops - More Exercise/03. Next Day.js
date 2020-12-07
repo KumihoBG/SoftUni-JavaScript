d1bc2ebda81031826faec05357e10045844bc66e
unknown <pavleta.taseva@gmail.com>
2020-12-07 21:40:29 +0200

    commit

--- 03. Basic Syntax, Conditional Statements and Loops - More Exercise/03. Next Day.js
+++ 03. Basic Syntax, Conditional Statements and Loops - More Exercise/03. Next Day.js
@@ -1,6 +1,6 @@
 function nextDay(year, month, day){
     let date = new Date(year, month - 1, day);
-    let tomorrow = new Date(year,month-1,date.getDate()+1);
+    let tomorrow = new Date(year, month - 1, date.getDate()+1);
     console.log(`${tomorrow.getFullYear()}-${tomorrow.getMonth()+1}-${tomorrow.getDate()}`);
 }
 
