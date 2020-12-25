5328baf48d4a228cd17f5ade2667ca67f2792afd
KumihoBG <you@example.com>
2020-12-25 13:15:53 +0200

    commit

--- 08. Arrays - Exercise/06. Equal Sums.js
+++ 08. Arrays - Exercise/06. Equal Sums.js
@@ -1,4 +1,25 @@
+function equalSums(arr) {
+    let hasEqualSum = false;
 
+    for (let i = 0; i < arr.length; i++) {
+        let leftSum = 0;
+        let rightSum = 0;
+        for (let j = i - 1; j >= 0; j--) {
+            leftSum += arr[j];
+        }
+        for (let j = i + 1; j < arr.length; j++) {
+            rightSum += arr[j];
+
+        }
+        if (leftSum === rightSum) {
+            console.log(i);
+            hasEqualSum = true;
+        }
+    }
+    if (!hasEqualSum) {
+        console.log('no');
+    }
+}
 
 equalSums([1, 2, 3, 3]);
 equalSums([10, 5, 5, 99, 3, 4, 2, 5, 1, 1, 4]);
