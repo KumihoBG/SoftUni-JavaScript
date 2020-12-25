9db91a44898c6433c107efc7c39d596c797dffda
KumihoBG <you@example.com>
2020-12-25 14:37:39 +0200

    commit

--- 08. Arrays - Exercise/07. Max Sequence of Equal Elements.js
+++ 08. Arrays - Exercise/07. Max Sequence of Equal Elements.js
@@ -1,5 +1,28 @@
-function maxSequence(){
+function maxSequence(input) {
+    let newArr = [];
+    let counter = 0;
+    let winningCounter = 0;
+    let element = 0;
+    let number = "";
 
+    for (let i = 0; i < input.length - 1; i++) {
+        let current = input[i];
+        let next = input[i + 1];
+        if (current == next) {
+            counter++;
+            if (counter > winningCounter) {
+                winningCounter = counter;
+                element = input[i];
+                number = input[i];
+            }
+        } else {
+            counter = 0;
+        }
+    }
+    for (let j = 0; j <= winningCounter; j++) {
+        newArr.push(number);
+    }
+    console.log(newArr.join(' '));
 }
 
 maxSequence([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
