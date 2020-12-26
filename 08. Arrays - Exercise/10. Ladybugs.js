e46060ca6642ea4f96dfd15471167a24ec50350a
KumihoBG <you@example.com>
2020-12-26 16:23:38 +0200

    commit

--- 08. Arrays - Exercise/10. Ladybugs.js
+++ 08. Arrays - Exercise/10. Ladybugs.js
@@ -1,43 +1,74 @@
-function ladybugs(str){
-    let newArr = str.toString().split(',');
-    let fieldSize = Number(newArr[0]);
-    let indexes = newArr[1].toString().split(' ');  
-    let cell = 0;
-    let result = [];
-
-    for (let i = 2; i < newArr.length; i++){
-        let move = newArr[i].toString().split(' ');
-        let bugIndex = Number(move[0]);
-        let direction = move[1];
-        let flyLength = Number(move[2]);
-        for (let j = 0; j < indexes.length; j++){
-            let ladyBug = Number(indexes[j]);
-            
-            if (direction == "right"){
-                bugIndex += flyLength;
-                if (bugIndex >= fieldSize){
-                    bugIndex = 0;
-                    cell = 0;
-                    result.push(cell);
-                    break;
-                }
-                cell = 0;
-                result.push(cell);
-                if (bugIndex == ladyBug){
-                    bugIndex += 1;
-                    cell = 1;
-                    result.push(cell);
-                }
-                
-            }
-        }
-            
-    }
-    console.log(result.join(' '));
+function ladyBugs(arr) {
+	let sizeOfField = arr[0];
+	let ladybugsPositions = arr[1]
+		.split(' ')
+		.map(Number);
+	let ladybugsArray = [];
+ 
+	// fill the array with 0
+	for (let i = 0; i < sizeOfField; i++) {
+		ladybugsArray.push(0);
+	}
+	// mark starting ladybugs index
+	for (let i = 0; i < sizeOfField; i++) {
+		let ladybugIndex = ladybugsPositions[i];
+		if (ladybugIndex >= 0 && ladybugIndex < sizeOfField) {
+			ladybugsArray[ladybugIndex] = 1;
+		}
+	}
+ 
+	for (let i = 2; i < arr.length; i++) {
+		// JS destructuring
+		let [ladybugIndex, command, jumpLength] = arr[i].split(' ');
+		ladybugIndex = +ladybugIndex;
+		jumpLength = +jumpLength;
+ 
+		if (ladybugsArray[ladybugIndex] !== 1 || ladybugIndex < 0 || ladybugIndex >= ladybugsArray.length) {
+			continue;
+		}
+		// check for negative steps
+		if (jumpLength < 0) {
+			jumpLength = Math.abs(jumpLength);
+			if (command === 'right') {
+				command = 'left';
+			} else if (command === 'left') {
+				command = 'right';
+			}
+		}
+		// Free Position
+		ladybugsArray[ladybugIndex] = 0;
+		if (command === 'right') {
+			// Ladybug jumps one time
+			let newPosition = ladybugIndex + jumpLength;
+			// Jump another time if there is a lady bug
+			if (ladybugsArray[newPosition] === 1) {
+				newPosition = newPosition + jumpLength;
+			}
+			if (newPosition <= ladybugsArray.length) {
+				ladybugsArray[newPosition] = 1;
+			}
+ 
+		} else {
+			// Lady bug jumps to the left
+			let newPosition = ladybugIndex - jumpLength;
+			// Jump another time if there is a lady bug
+			if (ladybugsArray[newPosition] === 1) {
+				newPosition = newPosition - jumpLength;
+			}
+			if(newPosition >= 0 ){
+				ladybugsArray[newPosition] = 1;
+			}
+		}
+ 
+	}
+ 
+	console.log(ladybugsArray.join(' '));
+ 
 }
 
-// ladybugs([ 3, '0 1','0 right 1','2 right 1' ]);
-ladybugs([ 3, '0 1 2',
-'0 right 1',
-'1 right 1',
-'2 right 1']);
+ladyBugs(	[ 5, '3',
+	'3 left 2',
+	'1 left -2']
+);
+
+
