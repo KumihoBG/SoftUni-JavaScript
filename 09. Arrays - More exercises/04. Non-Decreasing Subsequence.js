72e3448ec570cbd66b433f3e45fc2738a6c362b5
KumihoBG <you@example.com>
2020-12-29 15:52:17 +0200

    commit

--- 09. Arrays - More exercises/04. Non-Decreasing Subsequence.js
+++ 09. Arrays - More exercises/04. Non-Decreasing Subsequence.js
@@ -1,25 +1,13 @@
-function maxSequence(input) {
-    let newArr = [];
-    newArr.push(input[0]);
-    let max = 0;
-    let totalMaxNum = 0;
-
-    for (let i = 1; i < input.length; i++) {
-        let current = input[i];
-        let next = input[i + 1];
-        let previous = input[i - 1];
-        if (current > previous){
-            max = current;
-        } else {
-            max = previous;
+function maxSequence(arr) {
+    let max = arr[0];
+    let result = arr.filter(el => {
+        if (el >= max) {
+            max = el;
         }
-        if (next > current) {
-            max = next;
-            newArr.push(max);
-        }
-    }
-    console.log(newArr.join(' '));
+        return el >= max;
+    });
+    console.log(result.join(' '));
 }
 
 maxSequence([1, 3, 8, 4, 10, 12, 3, 2, 24]);
-// maxSequence([20, 3, 2, 15, 6, 1]);
+maxSequence([20, 3, 2, 15, 6, 1]);
