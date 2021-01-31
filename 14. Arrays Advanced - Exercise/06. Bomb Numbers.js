00ed0c8dc992732e6ac23e61d577ee1655b8e8dd
KumihoBG <you@example.com>
2021-01-31 14:51:59 +0200

    commit

--- 14. Arrays Advanced - Exercise/06. Bomb Numbers.js
+++ 14. Arrays Advanced - Exercise/06. Bomb Numbers.js
@@ -1,27 +1,21 @@
-function bombNus(arr, bomb) {
-    let num = bomb[0];
-    let bombPower = bomb[1];
-    let index = arr.indexOf(num);
-    let afterIndex = index + bombPower;
-    let start = arr.splice(0, index - bombPower);
-    let end = arr.splice(afterIndex);
-    let result = start.concat(end);
-    let sum = 0;
-    isIncluded(arr);
-    isIncluded(result);
-        
-    function isIncluded(arr){
-        if (arr.includes(num)){
-            arr.splice(index, 1);
-            for (let i = 0; i < result.length; i++){
-                let current = Number(result[i]);
-                sum += current;
-            }
-        }
+function bombNus(array, detonation) {
+    while (array.includes(detonation[0])) {
+        let bombPosition = array.indexOf(detonation[0]);
+        let bombPower = detonation[1];
+        let index = bombPosition - bombPower;
+
+        if (index < 0) {
+            index = 0;
+        } 
+        array.splice(bombPosition, bombPower + 1);
+        array.splice(index, bombPower);
     }
-    console.log(sum);;
+    let result = array.reduce((acc, val) => acc + val, 0);
+    return result;
+
 }
 
-// bombNus([1, 2, 2, 4, 2, 2, 2, 9], [4, 2]);
-// bombNus([1, 4, 4, 2, 8, 9, 1], [9, 3]);
-bombNus([1, 1, 2, 1, 1, 1, 2, 1, 1, 1], [2, 1]);
+console.log(bombNus([1, 2, 2, 4, 2, 2, 2, 9], [4, 2]));
+console.log(bombNus([1, 4, 4, 2, 8, 9, 1], [9, 3]));
+console.log(bombNus([1, 1, 2, 1, 1, 1, 2, 1, 1, 1], [2, 1]));
+console.log(bombNus([1, 7, 7, 1, 2, 3], [7, 1]));
