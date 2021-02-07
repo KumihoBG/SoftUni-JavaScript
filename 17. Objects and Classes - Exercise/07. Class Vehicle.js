133c07c0903cc13168e4cb5557dd8d8d3895ce95
KumihoBG <you@example.com>
2021-02-07 19:55:16 +0200

    commit

--- 17. Objects and Classes - Exercise/07. Class Vehicle.js
+++ 17. Objects and Classes - Exercise/07. Class Vehicle.js
@@ -1,31 +1,22 @@
-function vehicle(){
-    class Vehicle {
-        constructor(type, model, parts, fuel){
-            this.type = type;
-            this.model = model;
-            this.parts = parts;
-            this.fuel = fuel;
-        }
-        parts = {
-            engine: Number(engine),
-            power: Number(power),
-            quality: engine * fuel
-        };
-
-        drive(fuelLoss) {
-            return fuel -= fuelLoss;
-        }
+function vehicle() {
+  class Vehicle {
+    constructor(type, model, parts, fuel) {
+      this.type = type;
+      this.model = model;
+      this.parts = parts;
+      this.fuel = fuel;
+      this.parts.quality = this.parts.engine * this.parts.power;
     }
+    drive(loss) {
+      return (this.fuel = this.fuel - loss);
+    }
+  }
 
-    let parts = { 
-        engine: 6, 
-        power: 100, 
-        quality: engine * power,
-    };
-    let vehicle = new Vehicle('a', 'b', parts, 200);
-    vehicle.drive(100);
-    console.log(vehicle.fuel);
-    console.log(vehicle.parts.quality);
+  let parts = { engine: 6, power: 100 };
+  let vehicle = new Vehicle("a", "b", parts, 200);
+  vehicle.drive(100);
+  console.log(vehicle.fuel);
+  console.log(vehicle.parts.quality);
 }
 
 vehicle();
