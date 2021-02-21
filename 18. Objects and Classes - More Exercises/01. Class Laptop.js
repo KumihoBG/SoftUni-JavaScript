e15357d47641d7d74910320c5a76501d3560914a
KumihoBG <you@example.com>
2021-02-21 14:07:24 +0200

    commit

---
+++ 18. Objects and Classes - More Exercises/01. Class Laptop.js
@@ -0,0 +1,13 @@
+function laptop(input){
+    class Laptop {
+        constructor(producer, age, brand){
+            this.info = {
+                producer, 
+                age,
+                brand
+            }
+        }
+    }
+}
+
+laptop();
