824fa13bc9c1f1e14489fa88f596473576932e2e
KumihoBG <you@example.com>
2021-02-28 15:56:27 +0200

    commit

--- 18. Objects and Classes - More Exercises/01. Class Laptop.js
+++ 18. Objects and Classes - More Exercises/01. Class Laptop.js
@@ -1,13 +1,47 @@
-function laptop(input){
+function laptop(input) {
     class Laptop {
-        constructor(producer, age, brand){
+        constructor(producer, age, brand) {
             this.info = {
-                producer, 
+                producer,
                 age,
                 brand
             }
+            this.isOn = false;
+            this.quality = 0;
+        }
+
+        get price() {
+            return 800 - this.age * 2 + this.quality * 0.5
+        }
+        turnOn() {
+            this.isOn = true
+            return this.isOn
+        }
+        turnOff() {
+            this.isOn = false
+            return this.isOn
+        }
+        showInfo() {
+            return JSON.stringify(this.producer, this.age, this.brand);
         }
     }
+    const Laptop = result;
+
+    let laptop = new Laptop({ producer: "Dell", age: 2, brand: "XPS" }, 10);
+
+    laptop.turnOn()
+    laptop.turnOff()
+    laptop.turnOn()
+    console.log(laptop);
 }
 
-laptop();
+laptop(let info = {producer: "Dell", age: 2, brand: "XPS"}
+let laptop = new Laptop(info, 10)
+laptop.turnOn()
+console.log(laptop.showInfo())
+laptop.turnOff()
+console.log(laptop.quality)
+laptop.turnOn()
+console.log(laptop.isOn)
+console.log(laptop.price)
+);
---
+++ 19. Associative Arrays - Lab/01. Phone Book.js
@@ -0,0 +1,11 @@
+function solve(input) {
+    for (const line of input) {
+        let [name, phone] = line.split(' ');
+    }
+}
+
+solve(['Tim 0834212554',
+'Peter 0877547887',
+'Bill 0896543112',
+'Tim 0876566344']
+);
