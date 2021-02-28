0c356e68d5972e61302f1064f256f4c93d32a853
KumihoBG <you@example.com>
2021-02-28 16:14:17 +0200

    commit

--- 19. Associative Arrays - Lab/Objects-Cheatsheet.js
+++ 19. Associative Arrays - Lab/Objects-Cheatsheet.js
@@ -1,10 +1,13 @@
+// Iteration through objects and associative arrays
 function solve(input) {
     let contacts = {};
 
     for (const line of input) {
         let [name, phone] = line.split(' ');
+        // assigning key and value to the empty object
         contacts[name] = phone;
     }
+    // Accessing all object's keys and printing them plus their values
     Object.keys(contacts).forEach(key => console.log(`${key} -> ${contacts[key]}`));
 }
 
@@ -15,13 +18,8 @@ function solve(input) {
         let phone = contact.split(' ')[1];
         contacts[name] = phone;
     }
+    // iteration of objects - for in only!
     for (const key in contacts) {
         console.log(`${key} -> ${contacts[key]}`);
     }
 }
-
-solve(['Tim 0834212554',
-'Peter 0877547887',
-'Bill 0896543112',
-'Tim 0876566344']
-);
