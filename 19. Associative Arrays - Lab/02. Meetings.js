8a1f6c14968838b1422b00ffb3d322c9a2bfd920
KumihoBG <you@example.com>
2021-02-28 17:50:14 +0200

    commit

---
+++ 19. Associative Arrays - Lab/02. Meetings.js
@@ -0,0 +1,19 @@
+function solve(arrStr){
+    let items = new Map();
+
+    arrStr.forEach(line => {
+        let [days, name] = arrStr.split(' ');
+    });
+    for (const line of arrStr) {
+        let [days, name] = arrStr.split(' ');
+        items.set()
+    }
+    
+    
+}
+
+solve(['Monday Peter',
+'Wednesday Bill',
+'Monday Tim',
+'Friday Tim']
+);
