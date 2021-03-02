eefd3a8b8fc3f1bf549b94b377237e968564727e
KumihoBG <you@example.com>
2021-03-02 09:22:58 +0200

    commit

---
+++ 20. Associative Arrays - Exercise/WordsTracker.js
@@ -0,0 +1,17 @@
+function wordsTracker(input) {
+    let text = input.slice();
+    let occurences = getWantedWords(input);
+    let count = 1;
+
+    function getWantedWords(arr){
+        input.shift().split(' ').forEach(word => {
+            
+        });;
+    }
+
+}
+
+wordsTracker([
+    'this sentence', 'In','this','sentence','you','have','to','count','the','occurances','of','the'
+    ,'words','this','and','sentence','because','this','is','your','task'
+    ]);
